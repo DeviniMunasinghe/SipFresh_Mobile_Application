ec5792be6a220d81b60d7cf16f41956a74146500
Devini Munasinghe <devinimadumali@gmail.com>
2025-01-27 16:42:19 +0530

    implement code to delete an admin by user is

--- backend/api/controllers/auth-controller.js
+++ backend/api/controllers/auth-controller.js
@@ -171,3 +171,20 @@ exports.getAllAdmins = async (req, res) => {
     });
   }
 };
+
+//delete an admin
+exports.deleteAdmin = async (req, res) => {
+  const { user_id } = req.params;
+  try {
+    await User.deleteById(user_id);
+    res.status(200).json({
+      message: "Admin deleted successfully",
+    });
+  } catch (error) {
+    console.error("Error deleting admin:", error);
+    res.status(500).json({
+      message: "server error",
+      error: error.message,
+    });
+  }
+};
