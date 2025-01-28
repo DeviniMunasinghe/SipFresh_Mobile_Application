f0d757762bf0bf90d5c14433d13c3b5bb0d03bc0
Devini Munasinghe <devinimadumali@gmail.com>
2025-01-28 16:55:56 +0530

    update controller for get all registered users

--- backend/api/controllers/auth-controller.js
+++ backend/api/controllers/auth-controller.js
@@ -188,3 +188,20 @@ exports.deleteAdmin = async (req, res) => {
     });
   }
 };
+
+// Fetch all registered users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAllUsers();
+    res.status(200).json({
+      status: "success",
+      data: users,
+    });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
