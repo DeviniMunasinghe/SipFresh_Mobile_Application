23258aad0dcdaf4533da3b47650dd60fc3956a23
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-02-15 23:39:45 +0530

    OrderSuccessDialog

---
+++ frontend/lib/widgets/order_success_dialog.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OrderSuccessDialog extends StatelessWidget {
+  const OrderSuccessDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: const Text('Order Success Dialog'),
+    );
+  }
+}
