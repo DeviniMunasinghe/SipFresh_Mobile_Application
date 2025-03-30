3b0f1c0699b2f449e5c729218f04a313e93a9526
PawaniMekhala <pawanimekhala3@gmail.com>
2025-03-31 00:12:37 +0530

    feat: add SplashScreen to main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(), // Set BottomNavBar as the main layout
+      home: const SplashScreen(), // Set splashscreen as the main layout
     );
   }
 }
