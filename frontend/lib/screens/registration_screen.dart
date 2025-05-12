3de8175839f226eb2c0f356b3154a0cfca6a2012
PawaniMekhala <pawanimekhala3@gmail.com>
2025-05-12 23:22:42 +0530

    feat: add login button with navigation to login screen

--- frontend/lib/screens/registration_screen.dart
+++ frontend/lib/screens/registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
 
 class RegistrationScreen extends StatelessWidget {
   const RegistrationScreen({super.key});
@@ -27,7 +28,24 @@ class RegistrationScreen extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   color: Color(0xFF82BA53),
                 ),
-              )
+              ),
+
+              SizedBox(height: 20),
+              // login button with navigation to login screen
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF82BA53),
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                },
+                child: Text("Log In", style: TextStyle(color: Colors.white)),
+              ),
             ],
           ),
         ),
