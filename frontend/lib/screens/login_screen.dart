c518bbdef4174bd7f7796c8a7e21bb9f92b24414
PawaniMekhala <pawanimekhala3@gmail.com>
2025-05-17 12:36:44 +0530

    feat: add password field to sign in screen

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  bool _isPasswordVisible = false;
+  bool _rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +54,37 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 15),
+
+              // password field
+              Text(
+                "Password",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 5),
+              TextField(
+                obscureText: !_isPasswordVisible,
+                decoration: InputDecoration(
+                  hintText: "Type your password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  filled: true,
+                  fillColor: Colors.white,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                    icon: Icon(_isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10)
             ],
           ),
         ),
