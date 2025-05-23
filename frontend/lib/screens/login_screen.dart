be0f1752fc4c2d4e56272975496f18d5e762b335
PawaniMekhala <pawanimekhala3@gmail.com>
2025-05-23 13:59:42 +0530

    feat: add form validation and error handling to LoginScreen

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'signup_screen.dart';
-// import 'home_screen.dart';
+import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,8 +9,36 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   bool _isPasswordVisible = false;
   bool _rememberMe = false;
+
+  final _formKey = GlobalKey<FormState>();
+  String? _errorMessage;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _handleLogin() {
+    if (_formKey.currentState!.validate()) {
+      // Clear any previous error message
+      setState(() => _errorMessage = null);
+
+      // Navigate to home screen
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    } else {
+      setState(() => _errorMessage = 'Please enter valid email and password');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,95 +54,142 @@ class LoginScreenState extends State<LoginScreen> {
           ),
         ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: [
-          Text(
-            "Welcome back!",
-            style: TextStyle(
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            Text(
+              "Welcome back!",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            "Email",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 5),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Type your email",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              filled: true,
-              fillColor: Colors.white,
-            ),
-          ),
-          SizedBox(height: 15),
+            SizedBox(height: 20),
 
-          // password field
-          Text(
-            "Password",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+            // Email Field
+            Text(
+              "Email",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-          ),
-          SizedBox(height: 5),
-          TextField(
-            obscureText: !_isPasswordVisible,
-            decoration: InputDecoration(
-              hintText: "Type your password",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              filled: true,
-              fillColor: Colors.white,
-              suffixIcon: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _isPasswordVisible = !_isPasswordVisible;
-                  });
-                },
-                icon: Icon(_isPasswordVisible
-                    ? Icons.visibility
-                    : Icons.visibility_off),
+            SizedBox(height: 5),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                hintText: "Type your email",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Email is required';
+                } else if (!RegExp(r'^[\w-.]+@([\w-]+\.)+[\w]{2,4}$')
+                    .hasMatch(value)) {
+                  return 'Enter a valid email';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 15),
+
+            // password field
+            Text(
+              "Password",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          SizedBox(height: 10),
-          // remember me and forgot password row
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Checkbox(
-                    value: _rememberMe,
-                    activeColor: Colors.green,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        _rememberMe = value ?? false;
-                      });
-                    },
-                  ),
-                  Text("Remember me",
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                ],
-              ),
-              TextButton(
-                onPressed: () {
-                  // Forgot Password Action
-                },
-                child: Text(
-                  "Forgot password?",
-                  style: TextStyle(color: Colors.grey),
+            SizedBox(height: 5),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                hintText: "Type your password",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                filled: true,
+                fillColor: Colors.white,
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                  icon: Icon(_isPasswordVisible
+                      ? Icons.visibility
+                      : Icons.visibility_off),
                 ),
               ),
-            ],
-          ),
-        ],
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Password is required';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 10),
+
+            // remember me and forgot password row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Checkbox(
+                      value: _rememberMe,
+                      activeColor: Colors.green,
+                      onChanged: (bool? value) {
+                        setState(() => _rememberMe = value ?? false);
+                      },
+                    ),
+                    Text("Remember me",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Forgot Password Action
+                  },
+                  child: Text(
+                    "Forgot password?",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+
+            // Error message (if any)
+            if (_errorMessage != null)
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Text(
+                  _errorMessage!,
+                  style: const TextStyle(
+                      color: Colors.red, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+            SizedBox(height: 10),
+
+            // Login Button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF82BA53),
+                minimumSize: Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: _handleLogin,
+              child: Text("Log In", style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
       ),
     );
   }
