48cbb8079f95293b784b6f1ef52e9645494a0613
PawaniMekhala <pawanimekhala3@gmail.com>
2025-06-11 01:03:33 +0530

    feat: add searchbar to homescreen

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'order_details_screen.dart';
+import 'profile_screen.dart';
+import 'cart_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose(); // Clean up the controller
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,30 +27,92 @@ class HomeScreen extends StatelessWidget {
         title: const Text(
           "Sip Fresh",
           style: TextStyle(
-            fontSize: 22,
+            fontSize: 26,
             color: Colors.white,
           ),
         ),
+        // leading: Icon(Icons.home, color: Colors.white),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person_outline, color: Colors.white),
+            onPressed: () {
+              // Navigate to profile
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ProfileScreen()),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.shopping_cart_outlined, color: Colors.white),
+            onPressed: () {
+              // Navigate to cart
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const CartScreen()),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Colors.white),
+            onPressed: () {
+              // Navigate to notifications
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const NotificationScreen()),
+              // );
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // Navigates to the OrderDetailsScreen when the button is pressed
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const OrderDetailsScreen()),
-                );
-              },
-              child: const Text("Go to Order Details"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: Color(0xFFF5F5F5), // Light background
+              borderRadius: BorderRadius.circular(30),
             ),
-            const SizedBox(height: 10),
-          ],
-        ),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: 'What do you want to drink?',
+                border: InputBorder.none,
+                icon: Icon(Icons.search, color: Colors.grey),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.close, color: Colors.grey),
+                  onPressed: () {
+                    setState(() {
+                      _searchController.clear();
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigates to the OrderDetailsScreen when the button is pressed
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const OrderDetailsScreen()),
+                    );
+                  },
+                  child: const Text("Go to Order Details"),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
       ),
     );
   }
