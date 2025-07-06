73f4fdaadc53f7ab79d3abe054e0b39c8136b070
PawaniMekhala <pawanimekhala3@gmail.com>
2025-07-06 13:25:06 +0530

    feat: add searchbar and img slider to home screen

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'order_details_screen.dart';
 import 'profile_screen.dart';
 import 'cart_screen.dart';
@@ -65,54 +66,109 @@ class HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.all(16),
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            decoration: BoxDecoration(
-              color: Color(0xFFF5F5F5), // Light background
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'What do you want to drink?',
-                border: InputBorder.none,
-                icon: Icon(Icons.search, color: Colors.grey),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.close, color: Colors.grey),
-                  onPressed: () {
-                    setState(() {
-                      _searchController.clear();
-                    });
-                  },
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // searchbar
+            Container(
+              margin: const EdgeInsets.all(16),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F5), // Light background
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'What do you want to drink?',
+                  border: InputBorder.none,
+                  icon: Icon(Icons.search, color: Colors.grey),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.close, color: Colors.grey),
+                    onPressed: () {
+                      setState(() {
+                        _searchController.clear();
+                      });
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 10),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigates to the OrderDetailsScreen when the button is pressed
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const OrderDetailsScreen()),
-                    );
-                  },
-                  child: const Text("Go to Order Details"),
-                ),
+
+            const SizedBox(height: 10),
+
+            //img slider
+            CarouselSlider(
+              items: [
+                SliderImage(
+                    imagePath: 'assets/images/homeImg/SliderImage1.jpg'),
+                SliderImage(
+                    imagePath: 'assets/images/homeImg/SliderImage2.jpg'),
+                SliderImage(
+                    imagePath: 'assets/images/homeImg/SliderImage3.jpg'),
+                SliderImage(
+                    imagePath: 'assets/images/homeImg/SliderImage4.jpg'),
+                SliderImage(
+                    imagePath: 'assets/images/homeImg/SliderImage1.jpg'),
               ],
+
+              //Slider Container properties
+              options: CarouselOptions(
+                height: 200.0,
+                enlargeCenterPage: true,
+                autoPlay: true,
+                aspectRatio: 16 / 9,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enableInfiniteScroll: true,
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                viewportFraction: 0.8,
+              ),
             ),
-          ),
-          const SizedBox(height: 10),
-        ],
+
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigates to the OrderDetailsScreen when the button is pressed
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const OrderDetailsScreen()),
+                      );
+                    },
+                    child: const Text("Go to Order Details"),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SliderImage extends StatelessWidget {
+  final String imagePath;
+
+  const SliderImage({required this.imagePath, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(6.0),
+      height: 150,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        image: DecorationImage(
+          image: AssetImage(imagePath),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
